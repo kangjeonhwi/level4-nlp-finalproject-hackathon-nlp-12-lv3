cf315f024911e98e35f0b766105b9cdc5410c609
Junseong <hell-o-world@naver.com>
2025-01-21 14:14:50 +0900

    feat: salmonn evaluation을 진행할 때 SALMONN.generate() 함수를 이용하도록 변경
    중복된 코드를 SALMONN.generate()로 통합

--- evaluate_salmonn.py
+++ evaluate_salmonn.py
@@ -114,55 +114,14 @@ def main(args):
 
         # Preprocess
         samples = prepare_sample(samples, cuda_enabled=torch.cuda.is_available())
-        batch_size = samples["spectrogram"].shape[0]
-        spectrogram = samples["spectrogram"]
-        raw_wav = samples.get("raw_wav", None)
-        audio_padding_mask = samples.get("padding_mask", None)
-        speech_embeds, speech_atts = salmonn_preprocessor.encode_speech(spectrogram, raw_wav=raw_wav, audio_padding_mask=audio_padding_mask)
-
-        # Add prompt embeds + audio embed 
-        prompts = [test_prompt[task] for task in samples['task']]
-        templated_prompts = [cfg.config.model.prompt_template.format(prompt) for prompt in prompts]
-
-        speech_embeds, speech_atts = salmonn_preprocessor.prompt_wrap(speech_embeds, speech_atts, templated_prompts, multi_prompt=True)
-        bos = torch.ones(
-            [batch_size, 1],
-            dtype=torch.int32,
-            device=speech_embeds.device,
-        ) * tokenizer.bos_token_id
-
-        bos_embeds = llama_model.model.model.embed_tokens(bos)
-        atts_bos = speech_atts[:, :1]
-
-        embeds = torch.cat([bos_embeds, speech_embeds], dim=1)
-        attns = torch.cat([atts_bos, speech_atts], dim=1)
-
         generate_cfg = cfg.config.generate
-
-        # Generation
-        outputs = llama_model.model.generate(
-            inputs_embeds=embeds,
-            pad_token_id=llama_model.config.eos_token_id[0],
-            max_new_tokens=generate_cfg.get("max_new_tokens", 200),
-            num_beams=generate_cfg.get("num_beams", 4),
-            do_sample=generate_cfg.get("do_sample", False),
-            min_length=generate_cfg.get("min_length", 1),
-            temperature=generate_cfg.get("temperature", 1.0),
-            top_p=generate_cfg.get("top_p", 0.9),
-            repetition_penalty=generate_cfg.get("repetition_penalty", 1.0),
-            length_penalty=generate_cfg.get("length_penalty", 1.0),
-            attention_mask=attns,
-        )
-
-        results = tokenizer.batch_decode(outputs)
+        results = salmonn_preprocessor.generate(samples, generate_cfg=generate_cfg)
         hyp = [result.split(generate_cfg.end_sym)[0].lower() for result in results]
         hyps.extend(hyp)
 
         if not args.make_submission:
             ref = samples["text"]
             refs.extend(ref)
-
-    
 
     if args.make_submission:
         os.makedirs("submission_results", exist_ok=True)
