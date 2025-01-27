6a0a9ac80bb772391174316210a7a9128557539c
bullmouse <sjpark960520@gmail.com>
2025-01-27 11:41:03 +0900

    feat: init prune me dir

---
+++ prune_me/compute_block_similarity/layer_similarity.py
@@ -0,0 +1,131 @@
+import logging
+import csv
+import argparse
+import numpy as np
+import torch
+import datasets
+
+from torch.utils.data import DataLoader
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from tqdm import tqdm
+from peft import get_peft_model, LoraConfig, TaskType
+from utils import get_last_non_padded_tokens, compute_block_distances
+from typing import Optional
+
+logging.basicConfig(level=logging.INFO)
+
+# Set seed
+torch.manual_seed(42)
+np.random.seed(42)
+
+
+def main(model_path: str, dataset: str, dataset_column: str, batch_size: int, max_length: int,
+         layers_to_skip: int, dataset_size: Optional[int] = None, dataset_subset: Optional[str] = "eval"):
+    
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else: 
+        device = torch.device("cpu")
+
+    # if resource is a problem
+    # 일단 BitsAndBytesConfig는 MacOS에서는 지원하지 않기에 주석 처리
+    # quantization_config = BitsAndBytesConfig(load_in_4bit=True,
+    #                                         bnb_4bit_use_double_quant=True,
+    #                                         bnb_4bit_quant_type="nf4",
+    #                                         bnb_4bit_compute_dtype=torch.bfloat16)
+    
+    config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=True, r=4, lora_alpha=32, lora_dropout=0.1)
+    model = AutoModelForCausalLM.from_pretrained(model_path,  
+                                                device_map="auto", 
+                                                torch_dtype=torch.float32,
+                                                #  quantization_config=quantization_config, # macOS에서는 지원하지 않음
+                                                low_cpu_mem_usage=True,
+                                                output_hidden_states=True)
+    peft_model = get_peft_model(model, config)
+    peft_model.to(device)
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    if not tokenizer.pad_token:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    peft_model.eval()
+
+    dataset = datasets.load_dataset(dataset, split=dataset_subset)
+    if dataset_size:
+        dataset = dataset.select(range(dataset_size))
+
+    dataloader = DataLoader(dataset[dataset_column], batch_size=batch_size, shuffle=False, drop_last=True)
+
+    # Initialize a list to store distances for each block across the dataset
+    all_distances = [[] for _ in range(model.config.num_hidden_layers - layers_to_skip)]
+
+
+    for batch in tqdm(dataloader, desc="Processing batches"):
+        inputs = tokenizer(batch, return_tensors="pt", padding="longest", max_length=max_length, truncation=True).to(device)
+        with torch.no_grad():
+            outputs = peft_model(**inputs)
+        attention_mask = inputs["attention_mask"]
+        hidden_states = outputs.hidden_states
+        last_non_padded_hidden_states = get_last_non_padded_tokens(hidden_states, attention_mask)
+
+        # Remove the first element to account for the input layer not being considered a model hidden layer
+        # This adjustment is necessary for analyses focusing on the model's internal transformations
+        last_non_padded_hidden_states = last_non_padded_hidden_states[1:]
+        
+        # Ensure that the length of last_non_padded_hidden_states matches the number of model hidden layers minus one
+        assert len(last_non_padded_hidden_states) == peft_model.config.num_hidden_layers, "Length of last_non_padded_hidden_states  \
+        does not match expected number of hidden layers."
+
+        # Compute distances and append to all_distances
+        distances = compute_block_distances(last_non_padded_hidden_states, layers_to_skip)
+        for i, distance in enumerate(distances):
+            all_distances[i].append(distance)
+
+    # Calculate average distances for each block
+    average_distances = [np.mean(block_distances) for block_distances in all_distances]
+
+    # Write the average distances to a CSV file and compute the minimum average distance
+    min_distance = float('inf')  # Initialize with infinity
+    min_distance_layer = 0  # Initialize with an impossible value
+
+    with open('layer_distances.csv', 'w', newline='') as csvfile:
+        fieldnames = ['block_start', 'block_end', 'average_distance']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i, avg_dist in enumerate(average_distances):
+            # Write each row to the CSV
+            writer.writerow({
+                'block_start': i + 1,  # layer indices are 1-based in the paper
+                'block_end': i + 1 + layers_to_skip,
+                'average_distance': avg_dist
+            })
+            
+            if avg_dist < min_distance:
+                min_distance = avg_dist
+                min_distance_layer = i + 1  
+
+    # Log the layer with the minimum average distance
+    logging.info(f"Layer {min_distance_layer} to {min_distance_layer + layers_to_skip} has the minimum average distance of {min_distance}. Consider examining this layer more closely for potential optimization or removal.")
+    logging.info("Layer distances written to layer_distances.csv")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run model analysis.")
+    parser.add_argument("--model_path", type=str, required=True, help="Path to the model.")
+    parser.add_argument("--dataset", type=str, required=True, help="Name of the dataset.")
+    parser.add_argument("--dataset_column", type=str, required=True, help="The specific column of the dataset to use.")
+    parser.add_argument("--batch_size", type=int, default=1, help="Batch size for processing.")
+    parser.add_argument("--max_length", type=int, required=True, help="Maximum length of the tokenized input.")
+    parser.add_argument("--layers_to_skip", type=int, required=True, help="Number of layers to skip.")
+    parser.add_argument("--dataset_size", type=int, help="Optional argument to specify the size of the dataset.")
+    parser.add_argument("--dataset_subset", type=str, default="eval", help="Subset of the dataset to use (e.g., 'train', 'eval').")
+    parser.add_argument("--device", type=str, help="Device to run the model on ('cpu', 'cuda').")
+
+    args = parser.parse_args()
+
+    main(args.model_path, args.dataset, args.dataset_column, args.batch_size,
+         args.max_length, args.layers_to_skip, args.dataset_size, args.dataset_subset)
