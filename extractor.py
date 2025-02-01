b69547fa5233c708cf87780eee650bcbdc91f1a2
Junseong <hell-o-world@naver.com>
2025-02-02 01:53:36 +0900

    feat: extract 결과를 저장하도록 기능 추가

--- extractor.py
+++ extractor.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 import torch
 from runner import Runner
 
@@ -24,9 +25,15 @@ class Extractor(Runner):
         time_for_pooling = 0.0
         time_for_extraction = 0.0
         
-        prefix = loader.prefix
+        if mode == "train":
+            prefix = loader._dataloader.dataset.prefix
+            ann_path = loader._dataloader.dataset.ann_path
+        else:
+            prefix = loader.dataset.prefix
+            ann_path = loader.dataset.ann_path
         
         start_time = time.time()
+        new_data_json = []
         for sample in loader:
             
             spectrogram = sample["spectrogram"]
@@ -45,7 +52,42 @@ class Extractor(Runner):
             pooled_embeds = self.model._average_pooling(speech_embeds, audio_embeds)
             time_for_pooling += time.time() - start_time
             
-            tensor_path = os.path.join(prefix, f"{sample['id'][0]}.pt")
-            torch.save(pooled_embeds, tensor_path)
-            
+            start_time = time.time()
+            tensor_path = f"{sample['id'][0]}.pt"
+            torch.save(pooled_embeds, os.path.join(prefix, tensor_path))
+            new_data_json.append({
+                "path": sample['id'][0],
+                "feature": tensor_path,
+                "task": sample["task"][0],
+                "Q": sample["Q"][0],
+                "text": sample["text"][0],
+            })
             time_for_extraction += time.time() - start_time
+        
+        ann_path_splited = ann_path.split(".")
+        ann_path_splited[-2] += "_extracted"
+        new_ann_path = ".".join(ann_path_splited)
+        with open(os.path.join(prefix, new_ann_path), "w") as f:
+            json.dump({"annotation": new_data_json}, f, indent=4)
+        
+        ann_path_splited[-2] += "-meta"
+        meta_path = ".".join(ann_path_splited)
+        time_for_speech_processing /= len(loader)
+        time_for_audio_processing /= len(loader)
+        time_for_pooling /= len(loader)
+        time_for_extraction /= len(loader)
+        
+        with open(os.path.join(prefix, meta_path), "w") as f:
+            json.dump({
+                "time_for_speech_processing": time_for_speech_processing,
+                "time_for_audio_processing": time_for_audio_processing,
+                "time_for_pooling": time_for_pooling,
+                "time_for_extraction": time_for_extraction,
+                "samples" : len(loader)
+            }, f, indent=4)
+        
+        print(f"Time for speech processing (Avg.): {time_for_speech_processing:.2f}")
+        print(f"Time for audio processing (Avg.): {time_for_audio_processing:.2f}")
+        print(f"Time for pooling (Avg.): {time_for_pooling:.2f}")
+        print(f"Time for extraction (Avg.): {time_for_extraction:.2f}")
+        
